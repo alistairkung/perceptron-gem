03fd909f75ed7b7381344e95a673c079c48d4a30
terminalobject <doctorboredom@hotmail.com>
2017-08-24 11:51:01 +0100

    removed contained gems

--- lib/perceptron.rb
+++ lib/perceptron.rb
@@ -1,8 +1,25 @@
 require 'matrix'
 
 class Perceptron
+
   def self.create(features_number)
     @unit = Unit.new(features_number, Weight.new(features_number))
+  end 
+
+  def self.unit
+    @unit
+  end 
+  
+  def self.predictp(vector)
+    @unit.predict(vector)
+  end 
+
+  def self.train(hash)
+    @unit.train(hash)
+  end
+
+  def self.set_weight(array)
+    @unit.weight.update(array)
   end 
 end 
 
